1479d08e5ed02e0519d9affedee094ba21d66b29
Jonathan Zhou <jpz2022@mit.edu>
2022-01-11 22:44:49 -0500

    js

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb+srv://jzadmin:zhoupu2008@testcluster.apdsu.mongodb.net/class1125");
+//create data schema
+const ContactSchema ={
+    FirstName:String,
+    LastName:String,
+    Email:String,
+}
+const contact = mongoose.model("contact",ContactSchema);
+
+app.set('view engine','ejs');
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/show',(req,res)=>{
+    contact.find({},(err,contact)=>{
+        res.render('index',{
+            contactlist: contact
+        });
+    })
+})
+
+app.post("/",(req, res)=>{
+    let newContact = new contact({
+        FirstName: req.body.FirstName,
+        LastName: req.body.LastName,
+        Email: req.body.Email,
+    })
+    newContact.save();
+    res.redirect("/");
+});
+
+app.listen(8080, ()=>{
+    console.log('listening to port 8080')
+});
